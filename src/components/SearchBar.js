575ff76d64fcd3521b2c8ceb92e32f1bf3c8e75f
github username <github email address>
2020-02-02 13:55:02 -0500

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
+
+  
+
+
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={null} onChange={props.handleCheck} />
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" onChange={props.handleCheck}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(e) => props.handleFilter(e.target.value)}>
+          <option value="None">None</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,96 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state={
+    stocks: [],
+    porStocks: [],
+    sort: [],
+    filter: 'None'
+
+  }
+
+
+  handleFilter = (filter) => {
+    console.log(filter);
+
+
+    if(this.state.filter !== filter){
+      this.setState({filter: filter})
+    }
+
+    let newArray = filter === 'None'? this.state.stocks : this.state.stocks.filter(stock => stock.type === filter)
+    console.log(newArray);
+    
+    return this.handleSort(newArray)
+  }
+
+
+
+  handleSort = (Array) => {
+    if(this.state.sort.length < 1){
+      return <StockContainer stocks={Array} handleSave={this.handleSave}/>
+    }
+    let newArray = Array
+    if(this.state.sort.includes('Price')){
+      newArray = newArray.sort((a,b) => a.price - b.price)
+    }
+    if(this.state.sort.includes('Alphabetically')){
+      newArray = newArray.sort((a,b) =>{ return a.name.localeCompare(b.name) })
+    }
+    return <StockContainer stocks={newArray} handleSave={this.handleSave}/>
+
+  }
+
+
+
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(stocks => {
+      this.setState({
+        stocks: stocks
+      })
+    })
+  }
+
+  handleSave = (stock) => {
+    console.log(stock);
+    if(this.state.porStocks.includes(stock)){
+      let newArray = this.state.porStocks.filter(oldStock => oldStock.name !== stock.name )
+      this.setState({porStocks: newArray})
+    }else{
+      this.setState({
+        porStocks: [...this.state.porStocks, stock]
+      })
+    }
+    
+  }
+
+
+  handleCheck = (e) => {
+    console.log(e.target);
+    this.setState({
+      sort: [...this.state.sort, e.target.value]
+    })
+  }
+
+
+
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleCheck={this.handleCheck} handleFilter={this.handleFilter}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+           {this.handleFilter(this.state.filter)}
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer porStocks={this.state.porStocks} handleSave={this.handleSave}/>
 
             </div>
           </div>
